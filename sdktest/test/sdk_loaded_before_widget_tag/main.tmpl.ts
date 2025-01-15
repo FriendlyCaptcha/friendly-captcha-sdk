40b56ec441e5323902fde4bc42c11bb0e0770380
Rok Sušnik <rokostik@gmail.com>
2025-01-15 15:35:22 +0100

    test sdk loading before widget tag

--- sdktest/test/sdk_loaded_before_widget_tag/main.tmpl.ts
+++ sdktest/test/sdk_loaded_before_widget_tag/main.tmpl.ts
@@ -1,28 +1,25 @@
 /*!
- * Copyright (c) Friendly Captcha GmbH 2023.
+ * Copyright (c) Friendly Captcha GmbH 2025.
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
 import { sdktest } from "../../sdktestlib/sdk.js";
 
+sdktest.description(
+  "The site.js script is loaded before the widget tag that contains the EU API endpoint. After the widget tag is loaded, the EU agent should load and the widget should function normally.",
+);
+
 sdktest.test({ name: "one widget present" }, async (t) => {
   t.require.numberOfWidgets(1);
 });
 
-sdktest.test({ name: "none widget is not triggered by focus" }, async (t) => {
+sdktest.test({ name: "widget completes after focusing form" }, async (t) => {
   const w = t.getWidget()!;
+  const completePromise = t.assert.widgetCompletes(w);
 
   const ta: HTMLTextAreaElement = document.querySelector('input[type="textarea"]')!;
   ta.focus();
 
-  // Wait for the widget to be initialized
-  await new Promise((resolve) => setTimeout(resolve, 100));
-
-  if (w.getState() === "unactivated") {
-    // Already unactivated (init done)
-  } else {
-    const unactivatedPromise = t.assert.widgetInits(w);
-    await unactivatedPromise;
-  }
+  await completePromise;
 });
